8e9cf61ee31f8b713341285ac2141ab29e2c4b00
Julie Gouin <juliegouin@outlook.com>
2023-03-30 13:39:25 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <title>ClémentCoach</title>
+</head>
+<body>
+    <header>
+         <div class="header">
+            <a href="#" class="m-4">Se Connecter</a>
+        </div>
+    
+        <nav>
+            <div>
+                <img class="logo" src="assets/images/logo.png" alt="Logo">
+            </div>
+            <ul class="d-flex">
+                <li><a href="#">Home</a></li>
+                <li> / </li>
+                <li><a href="#">Cours</a></li>
+                <li> / </li>
+                <li><a href="#">Réservation</a></li>
+                <li> / </li>
+                <li><a href="#">Accueillir un cours</a></li>
+            </ul>
+           
+        </nav>
+    </header>
+
+    <footer>
+        <div class="footer">
+            <p class="m-2">07. 02. 68. 48. 72</p>
+            <p class="m-2">clementcoach@pilates.com</p>
+        </div>
+    </footer>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+</html>
+
